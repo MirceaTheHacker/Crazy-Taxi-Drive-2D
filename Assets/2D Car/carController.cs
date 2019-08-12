bf53e02e88f7867a192d175f622481818fc5223c
MirceaMatei <matei_mircea97@yahoo.com>
2019-08-12 21:01:52 +0300

    Respawn system in place

--- Assets/2D Car/carController.cs
+++ Assets/2D Car/carController.cs
@@ -1,86 +1,104 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class carController : MonoBehaviour {
+public class carController : MonoBehaviour
+{
+
+    public WheelJoint2D frontwheel;
+    public WheelJoint2D backwheel;
+
+    JointMotor2D motorFront;
+
+    JointMotor2D motorBack;
+
+    public float speedF;
+    public float speedB;
+
+    public float torqueF;
+    public float torqueB;
+
+
+    public bool TractionFront = true;
+    public bool TractionBack = true;
+
+
+    public float carRotationSpeed;
+
+    private Rigidbody2D m_Rigidbody2d { get { return GetComponent<Rigidbody2D>(); } }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+
+
+            if (TractionFront)
+            {
+                motorFront.motorSpeed = speedF * -1;
+                motorFront.maxMotorTorque = torqueF;
+                frontwheel.motor = motorFront;
+            }
+
+            if (TractionBack)
+            {
+                motorBack.motorSpeed = speedF * -1;
+                motorBack.maxMotorTorque = torqueF;
+                backwheel.motor = motorBack;
+
+            }
+
+        }
+        else if (Input.GetAxisRaw("Vertical") < 0)
+        {
+
+
+            if (TractionFront)
+            {
+                motorFront.motorSpeed = speedB * -1;
+                motorFront.maxMotorTorque = torqueB;
+                frontwheel.motor = motorFront;
+            }
+
+            if (TractionBack)
+            {
+                motorBack.motorSpeed = speedB * -1;
+                motorBack.maxMotorTorque = torqueB;
+                backwheel.motor = motorBack;
+
+            }
+
+        }
+        else
+        {
+
+            backwheel.useMotor = false;
+            frontwheel.useMotor = false;
+
+        }
 
 
 
 
-	public WheelJoint2D frontwheel;
-	public WheelJoint2D backwheel;
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
 
-	JointMotor2D motorFront;
+            GetComponent<Rigidbody2D>().AddTorque(carRotationSpeed * Input.GetAxisRaw("Horizontal") * -1);
 
-	JointMotor2D motorBack;
+        }
+    }
 
-	public float speedF;
-	public float speedB;
-
-	public float torqueF;
-	public float torqueB;
-
-
-	public bool TractionFront = true;
-	public bool TractionBack = true;
-
-
-	public float carRotationSpeed;
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-		if (Input.GetAxisRaw ("Vertical") > 0) {
-
-
-			if (TractionFront) {
-				motorFront.motorSpeed = speedF * -1;
-				motorFront.maxMotorTorque = torqueF;
-				frontwheel.motor = motorFront;
-			}
-
-			if (TractionBack) {
-				motorBack.motorSpeed = speedF * -1;
-				motorBack.maxMotorTorque = torqueF;
-				backwheel.motor = motorBack;
-
-			}
-
-		} else if (Input.GetAxisRaw ("Vertical") < 0) {
-
-
-			if (TractionFront) {
-				motorFront.motorSpeed = speedB * -1;
-				motorFront.maxMotorTorque = torqueB;
-				frontwheel.motor = motorFront;
-			}
-
-			if (TractionBack) {
-				motorBack.motorSpeed = speedB * -1;
-				motorBack.maxMotorTorque = torqueB;
-				backwheel.motor = motorBack;
-
-			}
-
-		} else {
-
-			backwheel.useMotor = false;
-			frontwheel.useMotor = false;
-
-		}
-
-
-
-
-		if (Input.GetAxisRaw ("Horizontal") != 0) {
-
-			GetComponent<Rigidbody2D> ().AddTorque (carRotationSpeed * Input.GetAxisRaw ("Horizontal") * -1);
-
-		}
-
-	}
+    internal void Respawn(Vector2 checkpoint)
+    {
+        gameObject.transform.position = checkpoint;
+        gameObject.transform.rotation = Quaternion.identity;
+        m_Rigidbody2d.velocity = Vector3.zero;
+    }
 }
---
+++ Assets/Scripts/HeadColliderHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadColliderHandler : MonoBehaviour
+{
+    private Vector2 m_CheckPoint;
+    private carController m_CarClass;
+
+    private void Start()
+    {
+        m_CheckPoint = gameObject.transform.position;
+        m_CarClass = GetComponentInParent<carController>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 8)
+        {
+            Debug.Log("Dead");
+            m_CarClass.Respawn(m_CheckPoint);
+        }
+    }
+}
