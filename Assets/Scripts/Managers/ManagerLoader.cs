08325bf6d430b8037c4875f6d71c5781f7987511
MirceaMatei <matei_mircea97@yahoo.com>
2019-08-13 21:24:19 +0300

    Moved more dependecies onto the Game Manager
    Added Game Manager loader onto Main Camera

---
+++ Assets/Scripts/Managers/ManagerLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerLoader : MonoBehaviour
+{
+    public GameObject gameManager;
+
+    private void Awake()
+    {
+        if (GameManager.Instance == null)
+        {
+            Instantiate(gameManager);
+        }
+    }
+}
