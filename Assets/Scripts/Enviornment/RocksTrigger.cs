88062eb5c3b9f2b6f7e0c007e20d0399694a68fc
MirceaMatei <matei_mircea97@yahoo.com>
2019-08-17 15:54:55 +0300

    Added rocks

---
+++ Assets/Scripts/Enviornment/RocksTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocksTrigger : MonoBehaviour
+{
+    public GameObject[] rocks;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            ActivateRocks();
+        }
+    }
+
+    private void ActivateRocks()
+    {
+        foreach (GameObject rock in rocks)
+        {
+            rock.SetActive(true);
+        }
+    }
+}
